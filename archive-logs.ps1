9c55c319c3080a43511abc102e83fc5d6654f346
Oleksandr Saurskyi <djasaa@gmail.com>
2025-01-30 00:39:18 +0200

    Create archive-logs.ps1

---
+++ archive-logs.ps1
@@ -0,0 +1,34 @@
+  param (
+    [string]$SourceFolder = $Env:SystemDrive+"\inetpub\logs\LogFiles",
+    [string]$DestinationFolder = "D:\IIS_Archived_Logs",
+    [string]$RuntimeLog = $env:SystemRoot+"\Logs\IISLogRotate.log",
+    [int]$Age = -30
+ )
+
+if (Test-Path $SourceFolder){
+    $Files = Get-ChildItem -Path $SourceFolder -Recurse -File | where {$_.LastWriteTime -lt (Get-Date).AddDays($Age)}
+
+    Start-Transcript -Append $RuntimeLog
+    foreach ($File in $Files)
+    {
+        $NewPath = $File.DirectoryName.Replace($SourceFolder,"")
+        if (!(Test-Path -LiteralPath "$DestinationFolder\$NewPath"))
+        {
+            New-Item -Path "$DestinationFolder\$NewPath" -ItemType Directory
+        }
+        $File | #| Copy-Item -Destination "$DestinationFolder\$NewPath" | 
+        Compress-Archive -Update -DestinationPath $DestinationFolder\$NewPath\Logs-$_$(get-date -f yyyy-MM-dd).zip | Remove-Item $File -Force
+        Write-Host $(Get-Date -format MM/dd/yy` hh:mm:ss) $File "Compressed to" $DestinationFolder\$NewPath\Logs-$_$(get-date -f yyyy-MM-dd).zip
+    }
+    foreach ($File in $Files)
+    {
+        $NewPath = $File.DirectoryName.Replace($SourceFolder,"")
+        Remove-Item $SourceFolder\$NewPath\$File -Force
+        Write-Host $(get-date -f yyyy-MM-dd) "File" $SourceFolder\$NewPath\$File "was successfully removed"
+    
+    }
+    Stop-Transcript
+} else
+{
+    Write-Host $SourceFolder does not exists. Nothing to cleanup.
+}
