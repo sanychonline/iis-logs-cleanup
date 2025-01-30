d4acd26bd037a9449e21208b7b7fa0952009e10a
Oleksandr Saurskyi <djasaa@gmail.com>
2025-01-30 14:33:21 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 curl --create-dirs -O --output-dir "C:\Users\Default\AppData\Local\Microsoft\Windows\PowerShell" https://raw.githubusercontent.com/sanychonline/iis-logs-cleanup/refs/heads/main/archive-logs.ps1
-schtasks /query /TN "IIS-Logs-Rotate" >NUL 2>&1 || schtasks.exe /create /tn "IIS-Logs-Rotate" /sc Weekly /d MON /st 00:00 /rl highest /ru system /tr "powershell.exe C:\Users\Default\AppData\Local\Microsoft\Windows\PowerShell\archive-logs.ps1"
+schtasks /query /TN "IIS-Logs-Rotate" >NUL 2>&1 || schtasks.exe /create /tn "IIS-Logs-Rotate" /sc Monthly /d 1 /st 00:00 /rl highest /ru system /tr "powershell.exe C:\Users\Default\AppData\Local\Microsoft\Windows\PowerShell\archive-logs.ps1"
